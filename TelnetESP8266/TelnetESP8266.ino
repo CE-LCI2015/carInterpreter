13aeca556c0df1d7f9a14d49c80186c671f3c308
fab2903 <fabsolano2903@outlook.es>
2015-11-20 19:38:34 -0600

    Se sube una version funcional del wifi.

--- TelnetESP8266/TelnetESP8266.ino
+++ TelnetESP8266/TelnetESP8266.ino
@@ -2,6 +2,7 @@
 
 SoftwareSerial BT1(11, 10); // RX | TX
 
+
 #define INPUT_SIZE 30
 #define FRONT_PIN 0
 #define BACK_PIN 1
@@ -9,80 +10,72 @@ SoftwareSerial BT1(11, 10); // RX | TX
 #define RIGHT_PIN 3
 #define LIGHT_PIN 1
 
-String W; char w;
+String B, C = "" ;
+char c = ' ' ;
+String W = "";
+char w ;
 
-void setup(){ 
-  
+void setup(){
   pinMode(FRONT_PIN, OUTPUT);
   pinMode(BACK_PIN, OUTPUT);
   pinMode(LEFT_PIN, OUTPUT);
   pinMode(RIGHT_PIN, OUTPUT);
   pinMode(LIGHT_PIN, OUTPUT);
-  
+
+
   Serial.begin(9600);
   BT1.begin(9600);
   pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
   delay(1500);
   SetUpWIFI() ;
 }
 
 void loop()
 {
-  if (BT1.available()) // Lo que entra por WIFI 
+  if (BT1.available()) // Lo que entra por WIFI - Serial
   { w = BT1.read() ;
-    W.concat(w);
+    W = W + w;
+    Serial.print(w); // Vamos montando un String con lo que entra
   }
-  if (w == '\n' || w == '\r') // Si han pulsado intro
+  if (Serial.available()) // Lo que entra por Serial - WIFI
+  { char s = Serial.read();
+    BT1.println(s);
+  }
+  if (w == '\n' || w == '\r') // Sin han pulsado ino
   {
-    Serial.println(W);
-    if(W.indexOf("MO")>=0)
-    {
-      Serial.print("\nMove ");
-      Serial.print(W.substring(2).toInt());
-      Serial.print(" blocks\n");
-      received();
-      
+    //Serial.println("Se ha detectado un intro");
+    //Serial.println(W);
+    //Serial.println("========================");
+    if ( W.indexOf("ON") > 0 ) // Si P13 esta incluido en el string
+    { digitalWrite( 13, HIGH) ;
+      Serial.println("\nTurning ON");
+      BT1.println("AT+CIPSEND=0,4\r");
+      delay(50);
+      BT1.println("REC\n\r");
+      delay(50);
+      BT1.flush();
     }
-    else if(W.indexOf("LE")>=0)
-    {
-      Serial.println("Turn Left");
-      received();
+    else if ( W.indexOf("OFF") > 0) // Si P13 esta incluido en el string
+    { digitalWrite( 13,LOW) ;
+      Serial.println("\nTurning OFF");
+      BT1.println("AT+CIPSEND=0,4\r");
+      delay(50);
+      BT1.println("REC\n\r");
+      delay(50);
+      BT1.flush();
     }
-    else if(W.indexOf("RI")>=0)
-    {
-      Serial.println("Turn right");
-      received();
+    else if ( W.length() >1){
+      //BT1.println(W);
+      //BT1.println("AT+CIPSEND=0,5\r");
+      //delay(50);
+      //BT1.println("ERRORC\r");
+      //delay(50);
+      BT1.flush();
     }
-    else if(W.indexOf("ON")>=0)
-    {
-      digitalWrite( 12, HIGH);
-      received();
-    }
-    else if(W.indexOf("OFF")>=0)
-    {
-      digitalWrite( 12, LOW);
-      received();
-    }
-    W = " "; w = ' ';
+    W = "" ; w = ' ' ;
   }
 }
-void printCommand(char*command, int args)
-{
-    Serial.print("\nCommand: ");
-    Serial.print(command);
-    Serial.print(", args:");
-    Serial.print(args);
-    Serial.print("\n");
-}
-void received()
-{
-    BT1.println("AT+CIPSEND=0,5\r");
-    delay(50);
-    BT1.print("REC\r\n");
-    delay(50);
-    BT1.flush();
-}
+
 String GetLineWIFI()
 { String S = "" ;
   if (BT1.available())
@@ -98,14 +91,14 @@ String GetLineWIFI()
 
 void SetUpWIFI()
 { String ordenes[] =
-  {
-    "AT",
-    "AT+CWMODE=3",
-    "AT+CWJAP=\"Fab2903\",\"Galaxys4\"",
-    "AT+CIPMUX=1",
-    "AT+CIPSERVER=1,23",
-    "END" // Para reconocer el fin de los comandos AT
-  };
+          {
+                  "AT",
+                  "AT+CWMODE=3",
+                  "AT+CWJAP=\"Fab2903\",\"Galaxys4\"",
+                  "AT+CIPMUX=1",
+                  "AT+CIPSERVER=1,23",
+                  "END" // Para reconocer el fin de los comandos AT
+          };
   int index = 0;
   while (ordenes[index] != "END")
   { BT1.println(ordenes[index++]);
@@ -122,6 +115,6 @@ void SetUpWIFI()
     }
     Serial.println("========");
   }
-    //BT1.println("AT+CIFSR");
+  //BT1.println("AT+CIFSR");
 
 }
